dfb3fd87486463af60b5ca879ffddb6af78c89d4
Hiteshmeher <meher.hitesh4@gmail.com>
2022-09-14 19:56:01 +0530

    [Hitesh]: Added code to create a payroll service database

---
+++ employeePayroll.sql
@@ -0,0 +1,23 @@
+mysql> create database payroll_services;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| payroll_services   |
+| performance_schema |
+| sakila             |
+| sampledatabase     |
+| sampledatabase2    |
+| sampledatabase3    |
+| sys                |
+| world              |
++--------------------+
+10 rows in set (0.00 sec)
+
+
+mysql> use payroll_services;
+Database changed
